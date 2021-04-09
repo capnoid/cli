5cdeb6b1a44ab8bd2105a0c70f75c6bb09b23b37
Joshua Ma <josh@airplane.dev>
2021-04-09 09:45:22 -0700

    Fix use of context.TODO() in configs (#69)

--- pkg/cmd/configs/configs.go
+++ pkg/cmd/configs/configs.go
@@ -1,8 +1,6 @@
 package configs
 
 import (
-	"context"
-
 	"github.com/MakeNowJust/heredoc"
 	"github.com/airplanedev/cli/pkg/cli"
 	"github.com/airplanedev/cli/pkg/cmd/auth/login"
@@ -23,7 +21,7 @@ func New(c *cli.Config) *cobra.Command {
 			$ airplane configs get my_config_name
 		`),
 		PersistentPreRunE: utils.WithParentPersistentPreRunE(func(cmd *cobra.Command, args []string) error {
-			return login.EnsureLoggedIn(context.TODO(), c)
+			return login.EnsureLoggedIn(cmd.Root().Context(), c)
 		}),
 	}
 
