903c45fe1dd94d8e3e5320fb3f0a10c4e16feea6
francescaleung <1069034+francescaleung@users.noreply.github.com>
2021-04-14 16:24:15 -0400

    Show parameter table instead of task arguments (#74)
    Inserts a parameter table instead of showing task arguments.
    
    ```
    $ airplane tasks list
         NAME     |    SLUG     | BUILDER |                          PARAMETERS                            
    --------------+-------------+---------+----------------------------------------------------------------
      Echo Loop   | echo_loop   | manual  |     NAME    |   SLUG    |   TYPE   | REQUIRED |   DEFAULT      
                  |             |         | ------------+-----------+----------+----------+--------------  
                  |             |         |   Count     | count     | integer  | true     |          25    
                  |             |         |   text      | text      | string   | true     | hello world    
                  |             |         |   long text | long_text | string   | false    |                
                  |             |         |   dry run   | dry_run   | boolean  | true     |                
                  |             |         |   since     | since     | datetime | true     |                
                  |             |         |                                                                
      Hello World | hello_world | manual  |
    ```

--- pkg/print/table.go
+++ pkg/print/table.go
@@ -3,6 +3,7 @@ package print
 import (
 	"encoding/json"
 	"strconv"
+	"strings"
 
 	"fmt"
 	"os"
@@ -41,20 +42,44 @@ func (t Table) apiKeys(apiKeys []api.APIKey) {
 func (t Table) tasks(tasks []api.Task) {
 	tw := tablewriter.NewWriter(os.Stdout)
 	tw.SetBorder(false)
-	tw.SetHeader([]string{"name", "slug", "builder", "arguments"})
+	tw.SetHeader([]string{"name", "slug", "builder", "parameters"})
+	tw.SetRowLine(true)
 
 	for _, t := range tasks {
 		var builder = t.Builder
-
 		if builder == "" {
 			builder = "manual"
+		}
+
+		var parametersStr string
+		if len(t.Parameters) > 0 {
+			pts := &strings.Builder{}
+			ptw := tablewriter.NewWriter(pts)
+			ptw.SetBorder(false)
+			ptw.SetHeader([]string{"name", "slug", "type", "required", "default"})
+			for _, p := range t.Parameters {
+				defaultStr := fmt.Sprintf("%v", p.Default)
+				if p.Default == nil {
+					defaultStr = ""
+				}
+
+				ptw.Append([]string{
+					p.Name,
+					p.Slug,
+					string(p.Type),
+					strconv.FormatBool(!p.Constraints.Optional),
+					defaultStr,
+				})
+			}
+			ptw.Render()
+			parametersStr = pts.String()
 		}
 
 		tw.Append([]string{
 			t.Name,
 			t.Slug,
-			t.Builder,
-			fmt.Sprintf("%v", t.Arguments),
+			builder,
+			parametersStr,
 		})
 	}
 
