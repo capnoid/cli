cdf7374bc66a638f2aed157d0ff9daad9fdf6171
Joshua Ma <josh@airplane.dev>
2021-04-14 22:30:59 -0700

    Fix logger colors to support format strings (#76)
    You can now call `logger.Gray("hello %s", "world")` - the library
    SprintFunc doesn't support templates and this was broken for some usages
    in `execute`.

--- pkg/logger/colors.go
+++ pkg/logger/colors.go
@@ -3,10 +3,10 @@ package logger
 import "github.com/fatih/color"
 
 var (
-	Gray   = color.New(color.FgHiBlack).SprintFunc()
-	Blue   = color.New(color.FgHiBlue).SprintFunc()
-	Red    = color.New(color.FgHiRed).SprintFunc()
-	Yellow = color.New(color.FgHiYellow).SprintFunc()
-	Green  = color.New(color.FgHiGreen).SprintFunc()
-	Bold   = color.New(color.Bold).SprintFunc()
+	Gray   = color.New(color.FgHiBlack).SprintfFunc()
+	Blue   = color.New(color.FgHiBlue).SprintfFunc()
+	Red    = color.New(color.FgHiRed).SprintfFunc()
+	Yellow = color.New(color.FgHiYellow).SprintfFunc()
+	Green  = color.New(color.FgHiGreen).SprintfFunc()
+	Bold   = color.New(color.Bold).SprintfFunc()
 )
