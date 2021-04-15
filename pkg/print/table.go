b03f7310405b6653965c12efd5e089e5db88412e
francescaleung <1069034+francescaleung@users.noreply.github.com>
2021-04-15 14:17:12 -0400

    Tweak table output for `tasks list` and `tasks get` (#79)
    ```
    $ airport tasks list
              NAME         |      SLUG      | BUILDER |             PARAMETERS              
    -----------------------+----------------+---------+-------------------------------------
      Access test          | access_test    | go      |                                     
    -----------------------+----------------+---------+-------------------------------------
      (AWS) Echo A Lot     | aws-echo-a-lot | manual  |                                     
    -----------------------+----------------+---------+-------------------------------------
      Check Weather        | check-weather  | manual  | - location* [string] (default: SF)  
    -----------------------+----------------+---------+-------------------------------------
      Create Airplane Team | create_team    | go      | - name* [string]                    
                           |                |         | - slug* [string]                    
                           |                |         | - domain [string]                   
    -----------------------+----------------+---------+-------------------------------------
      Create Task          | create-task    | manual  | - teamid* [string]                  
                           |                |         | - name* [string]                    
                           |                |         | - description* [string]             
    -----------------------+----------------+---------+-------------------------------------
      Echo Loop            | echo-loop      | manual  | - count* [integer] (default: 100)   
    -----------------------+----------------+---------+-------------------------------------
      Hello World          | hello_world    | node    | - name* [string] (default: World)   
    -----------------------+----------------+---------+-------------------------------------
      logsleep             | logsleep       | manual  |                                     
    -----------------------+----------------+---------+-------------------------------------
      Output Test          | output-test    | deno    |                                     
    -----------------------+----------------+---------+-------------------------------------
      Print Env            | env            | manual  |                                     
    -----------------------+----------------+---------+-------------------------------------
    * indicates a required parameter
    $ airport tasks get check-weather
    Name:     Check Weather
    Slug:     check-weather
    Builder:  manual
    
        NAME   |   SLUG   |     DESCRIPTION      |  TYPE  | REQUIRED | DEFAULT  
    -----------+----------+----------------------+--------+----------+----------
      Location | location | A location, e.g. NYC | string | yes      | SF       
    Task Parameters
    ```

--- pkg/print/table.go
+++ pkg/print/table.go
@@ -44,6 +44,8 @@ func (t Table) tasks(tasks []api.Task) {
 	tw.SetBorder(false)
 	tw.SetHeader([]string{"name", "slug", "builder", "parameters"})
 	tw.SetRowLine(true)
+	tw.SetAutoWrapText(false)
+	tw.SetCaption(true, "* indicates a required parameter")
 
 	for _, t := range tasks {
 		var builder = t.Builder
@@ -53,26 +55,21 @@ func (t Table) tasks(tasks []api.Task) {
 
 		var parametersStr string
 		if len(t.Parameters) > 0 {
-			pts := &strings.Builder{}
-			ptw := tablewriter.NewWriter(pts)
-			ptw.SetBorder(false)
-			ptw.SetHeader([]string{"name", "slug", "type", "required", "default"})
+			var ps []string
 			for _, p := range t.Parameters {
-				defaultStr := fmt.Sprintf("%v", p.Default)
-				if p.Default == nil {
-					defaultStr = ""
+				var reqStr string
+				if !p.Constraints.Optional {
+					reqStr = "*"
 				}
 
-				ptw.Append([]string{
-					p.Name,
-					p.Slug,
-					string(p.Type),
-					strconv.FormatBool(!p.Constraints.Optional),
-					defaultStr,
-				})
+				var defaultStr string
+				if p.Default != nil {
+					defaultStr = fmt.Sprintf(" (default: %v)", p.Default)
+				}
+
+				ps = append(ps, fmt.Sprintf("- %s%s [%s]%s", p.Slug, reqStr, string(p.Type), defaultStr))
 			}
-			ptw.Render()
-			parametersStr = pts.String()
+			parametersStr = strings.Join(ps, "\n")
 		}
 
 		tw.Append([]string{
@@ -88,7 +85,45 @@ func (t Table) tasks(tasks []api.Task) {
 
 // Task implementation.
 func (t Table) task(task api.Task) {
-	t.tasks([]api.Task{task})
+	builderStr := task.Builder
+	if task.Builder == "" {
+		builderStr = "manual"
+	}
+
+	fmt.Fprintln(os.Stdout, "Name:    ", task.Name)
+	fmt.Fprintln(os.Stdout, "Slug:    ", task.Slug)
+	fmt.Fprintln(os.Stdout, "Builder: ", builderStr)
+	fmt.Fprintln(os.Stdout, "")
+
+	if len(task.Parameters) > 0 {
+		tw := tablewriter.NewWriter(os.Stdout)
+		tw.SetBorder(false)
+		tw.SetHeader([]string{"name", "slug", "description", "type", "required", "default"})
+		tw.SetCaption(true, "Task Parameters")
+
+		for _, p := range task.Parameters {
+			requiredStr := "yes"
+			if p.Constraints.Optional {
+				requiredStr = "no"
+			}
+
+			var defaultStr string
+			if p.Default != nil {
+				defaultStr = fmt.Sprintf("%v", p.Default)
+			}
+
+			tw.Append([]string{
+				p.Name,
+				p.Slug,
+				p.Desc,
+				string(p.Type),
+				requiredStr,
+				defaultStr,
+			})
+		}
+
+		tw.Render()
+	}
 }
 
 // Runs implementation.
