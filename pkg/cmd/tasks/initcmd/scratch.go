71221d9aa9e124794e3cff9d89e2d29dd14b08ee
Joshua Ma <josh@airplane.dev>
2021-04-19 12:21:45 -0700

    Add basic scaffolders for from-scratch init (#89)
    This adds Scaffolders which create basic (mostly empty) files for the various
    runtimes.
    
    For example, this creates package.json and requirements.txt so that you can
    deploy hello world tasks for Node and Python.
    
    The Go scaffolder still needs some work to produce go.sum and go.mod.

--- pkg/cmd/tasks/initcmd/scratch.go
+++ pkg/cmd/tasks/initcmd/scratch.go
@@ -2,10 +2,12 @@ package initcmd
 
 import (
 	"context"
+	"io/ioutil"
 	"os"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/airplanedev/cli/pkg/api"
+	"github.com/airplanedev/cli/pkg/cmd/tasks/initcmd/scaffolders"
 	"github.com/airplanedev/cli/pkg/logger"
 	"github.com/airplanedev/cli/pkg/taskdir"
 	"github.com/pkg/errors"
@@ -51,12 +53,13 @@ func initFromScratch(ctx context.Context, cfg config) error {
 		Description: description,
 	}
 
+	var scaffolder scaffolders.RuntimeScaffolder
 	if runtime == runtimeKindManual {
 		// TODO: let folks enter an image
 		def.Image = "alpine:3"
 		def.Command = []string{"echo", `"Hello World"`}
 	} else {
-		if def.Builder, def.BuilderConfig, err = defaultRuntimeConfig(runtime); err != nil {
+		if def.Builder, def.BuilderConfig, scaffolder, err = defaultRuntimeConfig(runtime); err != nil {
 			return err
 		}
 	}
@@ -65,8 +68,12 @@ func initFromScratch(ctx context.Context, cfg config) error {
 		return err
 	}
 
+	if err := writeRuntimeFiles(def, scaffolder); err != nil {
+		return err
+	}
+
 	logger.Log(`
-A skeleton Airplane task definition for '%s' has been created in %s! Fill it out with the rest of your task details.
+A skeleton Airplane task definition for '%s' has been created in %s, along with other starter files!
 
 Once you are ready, deploy it to Airplane with:
   airplane deploy -f %s`, name, file, file)
@@ -74,33 +81,33 @@ Once you are ready, deploy it to Airplane with:
 	return nil
 }
 
-func defaultRuntimeConfig(runtime runtimeKind) (string, api.BuilderConfig, error) {
+func defaultRuntimeConfig(runtime runtimeKind) (string, api.BuilderConfig, scaffolders.RuntimeScaffolder, error) {
 	// TODO: let folks configure the following configuration
 	switch runtime {
 	case runtimeKindDeno:
 		return "deno", api.BuilderConfig{
 			"entrypoint": "main.ts",
-		}, nil
+		}, scaffolders.DenoScaffolder{Entrypoint: "main.ts"}, nil
 	case runtimeKindDockerfile:
 		return "docker", api.BuilderConfig{
 			"dockerfile": "Dockerfile",
-		}, nil
+		}, scaffolders.DockerfileScaffolder{Dockerfile: "Dockerfile"}, nil
 	case runtimeKindGo:
 		return "go", api.BuilderConfig{
 			"entrypoint": "main.go",
-		}, nil
+		}, scaffolders.GoScaffolder{Entrypoint: "main.go"}, nil
 	case runtimeKindNode:
 		return "node", api.BuilderConfig{
 			"entrypoint":  "main.js",
 			"language":    "javascript",
 			"nodeVersion": "15",
-		}, nil
+		}, scaffolders.NodeScaffolder{Entrypoint: "main.js"}, nil
 	case runtimeKindPython:
 		return "python", api.BuilderConfig{
 			"entrypoint": "main.py",
-		}, nil
+		}, scaffolders.PythonScaffolder{Entrypoint: "main.py"}, nil
 	default:
-		return "", nil, errors.Errorf("unknown runtime: %s", runtime)
+		return "", nil, nil, errors.Errorf("unknown runtime: %s", runtime)
 	}
 }
 
@@ -153,4 +160,20 @@ func pickString(msg string, opts ...survey.AskOpt) (string, error) {
 	}
 
 	return str, nil
+}
+
+// For the various runtimes, we pre-populate basic versions of e.g. package.json to reduce how much
+// the user has to set up.
+func writeRuntimeFiles(def taskdir.Definition, scaffolder scaffolders.RuntimeScaffolder) error {
+	files := map[string][]byte{}
+	if err := scaffolder.GenerateFiles(def, files); err != nil {
+		return err
+	}
+	for filePath, fileContents := range files {
+		logger.Debug("writing file %s", filePath)
+		if err := ioutil.WriteFile(filePath, fileContents, 0664); err != nil {
+			return errors.Wrapf(err, "writing %s", filePath)
+		}
+	}
+	return nil
 }
